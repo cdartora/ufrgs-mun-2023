692347a0fd64d2ad2bb49808e55dc6e5eb5966f0
Carlos Dartora <carlosdalsoler@gmail.com>
2023-03-02 14:43:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <h1>Hello, world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
