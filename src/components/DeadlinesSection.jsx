7bac5be36f798958ab987b3ffe8cf8f4a59e17bb
Carlos Dartora <carlosdalsoler@gmail.com>
2023-05-15 23:54:30 -0300

    feat(carousel): fixes shape aspect ratio and categorize data folder

--- src/components/DeadlinesSection.jsx
+++ src/components/DeadlinesSection.jsx
@@ -1,38 +1,7 @@
 import CarouselCard from "./CarouselCard";
 import wave from "../assets/wave-2.svg";
 import calendar from "../assets/calendar.svg";
-const dates = [
-  {
-    month: "AGO",
-    day: "01",
-    name: "Lançamento matriz de países",
-  },
-  {
-    month: "AGO",
-    day: "05",
-    name: "Abertura das inscrições",
-  },
-  {
-    month: "SET",
-    day: "02",
-    name: "Prazo edital de benefícios",
-  },
-  {
-    month: "SET",
-    day: "23",
-    name: "Abertura do formulário B",
-  },
-  {
-    month: "SET",
-    day: "25",
-    name: "Lançamento guias de estudos",
-  },
-  {
-    month: "OUT",
-    day: "31",
-    name: "Primeiro dia do UFRGSMUN",
-  },
-];
+import deadlines from "../data/deadlines";
 
 function DeadlinesSection() {
   return (
@@ -49,7 +18,7 @@ function DeadlinesSection() {
           />
           <h4 className="text-lg font-extrabold text-zinc-200">Cronograma</h4>
           <div className="scroll-smooth p-5 mt-4 flex w-full gap-4 snap-x overflow-x-auto z-20 md:justify-center">
-            {dates.map((cardData) => (
+            {deadlines.map((cardData) => (
               <CarouselCard
                 cardData={cardData}
                 key={cardData.backgroundImage}
@@ -57,14 +26,14 @@ function DeadlinesSection() {
             ))}
           </div>
         </div>
+        <img
+          src={wave}
+          className="absolute w-full h-full opacity-20 z-10 top-0"
+          draggable="false"
+          style={{ userSelect: "none" }}
+          onmousedown="return false"
+        />
       </div>
-      <img
-        src={wave}
-        className="absolute w-full opacity-20 z-10 top-10"
-        draggable="false"
-        style={{ userSelect: "none" }}
-        onmousedown="return false"
-      />
     </div>
   );
 }
---
+++ src/data/deadlines.js
@@ -0,0 +1,34 @@
+const deadlines = [
+  {
+    month: "AGO",
+    day: "01",
+    name: "Lançamento matriz de países",
+  },
+  {
+    month: "AGO",
+    day: "05",
+    name: "Abertura das inscrições",
+  },
+  {
+    month: "SET",
+    day: "02",
+    name: "Prazo edital de benefícios",
+  },
+  {
+    month: "SET",
+    day: "23",
+    name: "Abertura do formulário B",
+  },
+  {
+    month: "SET",
+    day: "25",
+    name: "Lançamento guias de estudos",
+  },
+  {
+    month: "OUT",
+    day: "31",
+    name: "Primeiro dia do UFRGSMUN",
+  },
+];
+
+export default deadlines;
