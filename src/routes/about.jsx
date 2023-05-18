1a522e32060bc65aa4a22dea4c0a3003069f2dea
Carlos Dartora <carlosdalsoler@gmail.com>
2023-05-17 23:08:00 -0300

    feat(about): create the about router

---
+++ src/routes/about.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+export default function About() {
+  return (
+    <main className="App">
+      <header className="text-zinc-200">
+        <NavBar />
+      </header>
+
+      <Footer />
+    </main>
+  );
+}
