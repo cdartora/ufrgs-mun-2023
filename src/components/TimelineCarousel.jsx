60d5ffab4bc98be22589b14cfb67fb4b02d88eee
Carlos Dartora <carlosdalsoler@gmail.com>
2023-05-24 18:15:28 -0300

    feat(about): finish the styling of the history carousel

--- src/components/TimelineCarousel.jsx
+++ src/components/TimelineCarousel.jsx
@@ -1,21 +1,32 @@
-const years = [];
+import earth from "../assets/about/earth.svg";
 
+const years = [];
 for (let year = 2003; year < 2023; year++) {
   years.push(year);
 }
 
 function TimelineCarousel() {
   return (
-    <div className="scroll-smooth p-5 mt-4 flex w-full gap-4 snap-x overflow-x-auto z-20 md:justify-center">
-      {years.map((cardData, index) => (
-        <div
-          className="shadow-lg rounded-full w-12 h-12 bg-brand-dark p-5 flex justify-center items-center font-extrabold text-white text-xs text-center"
-          key={index}
-        >
-          {cardData}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="scroll-smooth p-5 mt-4 flex w-full gap-4 snap-x overflow-x-auto relative">
+        {years.map((cardData, index) => (
+          <div
+            className="cursor-pointer relative shadow-lg z-20 rounded-full w-12 h-12 bg-brand-dark p-5 flex justify-center items-center font-extrabold text-white text-xs text-center"
+            key={index}
+          >
+            {cardData}
+            <img
+              src={earth}
+              className="absolute z-10 overflow-hidden opacity-20 right-0"
+              draggable="false"
+              style={{ userSelect: "none" }}
+              onMouseDown={() => false}
+            />
+          </div>
+        ))}
+        <div className="h-1 bg-brand-dark absolute z-10 w-[1600px] top-[50%] translate-y-[-50%] " />
+      </div>
+    </>
   );
 }
 
--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -8,19 +8,42 @@ export default function About() {
     <main className="App">
       <header className="text-zinc-200">
         <NavBar />
-        <div className="max-w-screen-md p-8">
-          <div className="flex flex-col items-center mb-6">
-            <img
-              src={anniversaryLogo}
-              alt="Logo de aniversários dos 20 anos de UFRGSMUN"
-              className="w-7"
-            />
-            <h3 className="font-extrabold text-xl text-green-text">Sobre</h3>
+        <div>
+          <div className="max-w-screen-md p-8 mx-auto">
+            <div className="flex flex-col items-center mb-6">
+              <img
+                src={anniversaryLogo}
+                alt="Logo de aniversários dos 20 anos de UFRGSMUN"
+                className="w-7"
+              />
+              <h3 className="font-extrabold text-xl text-green-text">Sobre</h3>
+            </div>
+            <h4 className="font-extrabold text-sm text-black mb-4">
+              O que é um modelo da ONU?
+            </h4>
+            <p className="text-calendar-text text-xs font-light">
+              Um “Modelo das Nações Unidas” é uma conferência que proporciona
+              aos seus participantes uma simulação sobre temas importantes da
+              agenda internacional, de acordo com as regras de procedimento das
+              próprias reuniões das Nações Unidas. Durante a simulação, os
+              participantes atuarão como delegados de diversos países, com a
+              responsabilidade de sustentar suas posições ao longo das
+              discussões e defender seus interesses de acordo com suas
+              respectivas políticas externas. Além disso, os MUNs oferecem a
+              oportunidade de atuar, além de representantes diplomáticos, como
+              Chefes de Estado, Ministros, juízes ou mesmo como jornalistas no
+              comitê de imprensa internacional, cobrindo as discussões que
+              ocorrem nos comitês dos órgãos da ONU.
+            </p>
           </div>
-          <h4 className="font-extrabold text-sm text-black mb-4">
-            O que é um modelo da ONU?
+        </div>
+      </header>
+      <div className="bg-brand-dark">
+        <div className="max-w-screen-md p-8 text-right mx-auto">
+          <h4 className="font-extrabold text-sm text-lightTone mb-4">
+            E o UFRGSMUN?
           </h4>
-          <p className="text-calendar-text text-xs font-light">
+          <p className="text-lightTone text-xs font-light">
             Um “Modelo das Nações Unidas” é uma conferência que proporciona aos
             seus participantes uma simulação sobre temas importantes da agenda
             internacional, de acordo com as regras de procedimento das próprias
@@ -35,33 +58,17 @@ export default function About() {
             órgãos da ONU.
           </p>
         </div>
-      </header>
-      <div className="max-w-screen-md p-8 bg-brand-dark text-right">
-        <h4 className="font-extrabold text-sm text-lightTone mb-4">
-          E o UFRGSMUN?
-        </h4>
-        <p className="text-lightTone text-xs font-light">
-          Um “Modelo das Nações Unidas” é uma conferência que proporciona aos
-          seus participantes uma simulação sobre temas importantes da agenda
-          internacional, de acordo com as regras de procedimento das próprias
-          reuniões das Nações Unidas. Durante a simulação, os participantes
-          atuarão como delegados de diversos países, com a responsabilidade de
-          sustentar suas posições ao longo das discussões e defender seus
-          interesses de acordo com suas respectivas políticas externas. Além
-          disso, os MUNs oferecem a oportunidade de atuar, além de
-          representantes diplomáticos, como Chefes de Estado, Ministros, juízes
-          ou mesmo como jornalistas no comitê de imprensa internacional,
-          cobrindo as discussões que ocorrem nos comitês dos órgãos da ONU.
-        </p>
-      </div>
-      <div className="max-w-screen-md text-center py-8">
-        <h4 className="font-extrabold text-sm text-black mb-2">
-          Nossa História
-        </h4>
-        <p className="text-calendar-text text-xs font-light">
-          Clique nos anos para saber mais sobre nossa jornada até aqui!
-        </p>
-        <TimelineCarousel />
+        <div className="bg-white">
+          <div className="max-w-screen-md text-center py-8 mx-auto">
+            <h4 className="font-extrabold text-sm text-black mb-2">
+              Nossa História
+            </h4>
+            <p className="text-calendar-text text-xs font-light">
+              Clique nos anos para saber mais sobre nossa jornada até aqui!
+            </p>
+            <TimelineCarousel />
+          </div>
+        </div>
       </div>
       <div className="max-w-screen-md bg-brand-dark"></div>
       <Footer />
