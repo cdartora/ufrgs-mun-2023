c7406e7cc8d9a5d8ddc8ce65c40c73ee000d5b90
Carlos Dartora <carlosdalsoler@gmail.com>
2023-10-16 17:13:42 -0300

    feat(prepare): remove committees guidebook section

--- src/routes/Prepare.jsx
+++ src/routes/Prepare.jsx
@@ -3,60 +3,7 @@ import SectionContainer from "../components/SectionContainer";
 import { Title, Subtitle, Paragraph } from "../components/Formatting";
 
 import anniversaryLogo from "../assets/20-anos-white.svg";
-import divider from "../assets/divider-white-inverted.svg";
 import earth from "../assets/right-green-earth.svg";
-
-import CDHNU from "/committees/CDHNU.webp";
-import CELAC from "/committees/CELAC.webp";
-import CIJ from "/committees/CIJ.webp";
-import DPSUA from "/committees/DPS-UA.webp";
-import CESLA from "/committees/CES-LA.webp";
-import PRESS from "/committees/PRESS.webp";
-import UNSC from "/committees/UNSC.webp";
-import SPECPOL from "/committees/SPECPOL.webp";
-
-const studyBooks = [
-  {
-    name: "Conselho de Direitos Humanos das Nações Unidas",
-    img: CDHNU,
-    url: "",
-  },
-  {
-    name: "Comunidade de Estados Latino-Americanos e Caribenhos",
-    img: CELAC,
-    url: "",
-  },
-  {
-    name: "Corte Internacional",
-    img: CIJ,
-    url: "",
-  },
-  {
-    name: "Defesa, Proteção e Segurança da União Africana",
-    img: DPSUA,
-    url: "",
-  },
-  {
-    name: "Conselho Econômico e Social",
-    img: CESLA,
-    url: "",
-  },
-  {
-    name: "Imprensa Internacional",
-    img: PRESS,
-    url: "",
-  },
-  {
-    name: "The Special Political and Decolonization Committee",
-    img: SPECPOL,
-    url: "",
-  },
-  {
-    name: "The United Nations Security Council",
-    img: UNSC,
-    url: "",
-  },
-];
 
 function Prepare() {
   return (
@@ -95,42 +42,12 @@ function Prepare() {
           </div>
         </SectionContainer>
         <img
-          src={divider}
-          className="absolute w-full h-auto bottom-0 "
-          draggable="false"
-          style={{ userSelect: "none" }}
-          onMouseDown={() => false}
-        />
-        <img
           src={earth}
           className="absolute z-10 right-0 top-0 w-52 opacity-10"
           draggable="false"
           style={{ userSelect: "none" }}
           onMouseDown={() => false}
         />
-      </div>
-
-      <div className="text-almostWhite">
-        <SectionContainer className="lg:pt-0">
-          <div className="py-2 px-10 bg-darkGreen font-black rounded-full text-center">
-            Guia de estudos dos comitês
-          </div>
-          <div className="text-darkTone scroll-smooth pb-5 px-10 mt-4 flex w-full gap-10 snap-x overflow-x-auto ">
-            {studyBooks.map(({ name, img, url }) => (
-              <a
-                target="_blank"
-                href={url}
-                className="min-w-[200px] mt-4 text-lg font-bold hover:underline underline-offset-2"
-              >
-                <img
-                  src={img}
-                  className="h-48 w-48 snap-center rounded-xl shadow-lg shadow-black/10"
-                />
-                <a>{name}</a>
-              </a>
-            ))}
-          </div>
-        </SectionContainer>
       </div>
     </PageContainer>
   );
